984ced2666d9917dac57f9e5549c1f4465d10e4e
Keeko <charlesmontojo@gmail.com>
2018-03-19 10:13:01 +1100

    added gulp tasks to start with

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+const gulp = require('gulp'),
+sass = require('gulp-sass'),
+browserSync = require('browser-sync'),
+prefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src('app/assets/scss/main-styles.scss')
+  .pipe(sass())
+  .pipe(prefixer())
+  .pipe(gulp.dest('app/assets/styles'))
+  .pipe(browserSync.stream());
+});
+
+gulp.task('watch', ['sass'], function() {
+  browserSync.init({
+    server: './app'
+  });
+
+  gulp.watch('app/assets/scss/main-styles.scss', ['sass']);
+  gulp.watch('app/*.html').on('change', browserSync.reload);
+  gulp.watch('app/assets/scripts/*.js').on('change', browserSync.reload);
+});
+
+gulp.task('start', ['watch']);
